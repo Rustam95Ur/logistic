0762d88098a278a469b6ac72ade3b655e559186d
Rustam <rustam.95.ur@gmail.com>
2019-11-17 18:03:22 +0600

    Update header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,58 +4,54 @@
             <div class="row align-items-center">
                 <div class="col d-flex">
                     <div class="top-text">
-                        <small class="txt-black">Address</small>
+                        <small class="txt-black">{{trans('header.address')}}</small>
                         254 Street Avenue, LA US
                     </div>
                     <div class="top-text">
-                        <small class="txt-black">Emaii Us</small>
+                        <small class="txt-black">{{trans('header.email')}}</small>
                         <a href="#">support@logzee.com</a>
                     </div>
                     <div class="top-text">
-                        <small class="txt-black">Phone Number</small>
+                        <small class="txt-black">{{trans('header.phone')}}</small>
                         +88 (0) 202 0000 001
                     </div>
                 </div>
                 <div class="col-md-auto">
                     <!-- Topbar Language Dropdown Start -->
                     <div class="dropdown d-inline-flex lang-toggle shadow-sm">
-                        <a href="#" class="dropdown-toggle btn">
-                            <img src="{{asset('img/flag_us.png')}}" alt=""
-                                 class="dropdown-item-icon">
-                            <span class="d-inline-block d-lg-none">US</span>
-                            <span class="d-none d-lg-inline-block">United Kingdom</span> <i
-                                class="icofont-rounded-down"></i>
-                        </a>
-                        <div class="dropdown-menu dropdownhover-bottom dropdown-menu-right" role="menu">
-                            <a class="dropdown-item active" href="#">English</a>
-                            <a class="dropdown-item" href="#">Deutsch</a>
-                            <a class="dropdown-item" href="#">Español</a>
-                        </div>
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle-mob text-dark" href="index.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Русский <i class="icofont-rounded-down"></i></a>
+                                <ul class="dropdown-menu dropdownhover-bottom" aria-labelledby="dropdown03">
+                                    <li><a class="dropdown-item" href="index.html">English</a></li>
+                                </ul>
+                            </li>
+                        </ul>
                     </div>
                     <!-- Topbar Language Dropdown End -->
                     <div class="d-inline-flex request-btn ml-2">
                         <a class="btn-theme icon-left bg-orange no-shadow d-none d-lg-inline-block align-self-center"
                            href="#" role="button" data-toggle="modal"
-                           data-target="#request_popup"><i class="icofont-page"></i> Request Quote</a>
+                           data-target="#request_popup"><i class="icofont-page"></i> {{trans('header.request')}}</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- Main Navigation Start -->
-    <nav class="navbar navbar-expand-lg bg-transparent">
+    <nav class="navbar navbar-expand-lg bg-transparent-header">
         <div class="container text-nowrap bdr-nav px-0">
             <div class="d-flex mr-auto">
                 <a class="navbar-brand rounded-bottom light-bg" href="">
-                    <img src="{{asset('img/unnamed.png')}}" alt="">
+                    <img src="{{asset('img/logo-region.png')}}" alt="">
                 </a>
             </div>
             <!-- Topbar Request Quote Start -->
-            <span class="order-lg-last d-inline-flex request-btn">
-            <a class="nav-link mr-2 ml-auto" href="#" id="search_home"><i
-                    class="icofont-search"></i></a>
-        </span>
-            <!-- Toggle Button Start -->
+        {{--<span class="order-lg-last d-inline-flex request-btn">--}}
+        {{--<a class="nav-link mr-2 ml-auto" href="#" id="search_home"><i--}}
+        {{--class="icofont-search"></i></a>--}}
+        {{--</span>--}}
+        <!-- Toggle Button Start -->
             <button class="navbar-toggler x collapsed" type="button" data-toggle="collapse"
                     data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
                     aria-label="Toggle navigation" style="">
@@ -67,11 +63,11 @@
             <!-- Topbar Request Quote End -->
             <div class="collapse navbar-collapse" id="navbarCollapse" data-hover="dropdown">
                 <ul class="navbar-nav ml-auto  ">
-                    <li class="menu-item"><a title="" href="#">Home</a></li>
-                    <li class="menu-item"><a title="" href="#">About Us</a></li>
-                    <li class="menu-item"><a title="" href="#">Contact</a></li>
-                    <li class="menu-item"><a title="" href="#">Price</a></li>
-                    <li class="menu-item"><a title="" href="#">Blog</a></li>
+                    <li class="menu-item"><a title="" href="#">{{trans('header.home')}}</a></li>
+                    <li class="menu-item"><a title="" href="#">{{trans('header.about')}}</a></li>
+                    <li class="menu-item"><a title="" href="#">{{trans('header.services')}}</a></li>
+                    <li class="menu-item"><a title="" href="#">{{trans('header.cars')}}</a></li>
+                    <li class="menu-item"><a title="" href="#">{{trans('header.contacts')}}</a></li>
                 </ul>
                 <!-- Main Navigation End -->
             </div>
