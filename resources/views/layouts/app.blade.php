41e12d5b7ad2e7717b154fe8dc740c4a86c42de6
Rustam <rustam.95.ur@gmail.com>
2019-11-23 23:49:00 +0600

    Fix js code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,7 +96,32 @@
 <script type="text/javascript">
     jQuery(document).ready(function () {
         jQuery('#grid-container').cubeportfolio({
-            // options
+            filters: '#js-filters-masonry',
+            layoutMode: 'grid',
+            defaultFilter: '*',
+            animationType: 'slideDelay',
+            gapHorizontal: 20,
+            gapVertical: 20,
+            gridAdjustment: 'responsive',
+            mediaQueries: [{
+                width: 1500,
+                cols: 5
+            }, {
+                width: 1100,
+                cols: 4
+            }, {
+                width: 800,
+                cols: 3
+            }, {
+                width: 480,
+                cols: 2
+            }, {
+                width: 320,
+                cols: 1
+            }],
+            caption: 'overlayBottomAlong',
+            displayType: 'bottomToTop',
+            displayTypeSpeed: 100,
         });
     });
 </script>
